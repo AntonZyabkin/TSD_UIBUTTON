a78321c6ac9ff776f05fdd8da87123fd698d8a35
AntonZyabkin <zyabkinantonv@gmail.com>
2022-06-26 12:57:14 +0300

    second commit

--- UIButton/UIButton/ViewController.swift
+++ UIButton/UIButton/ViewController.swift
@@ -15,24 +15,53 @@ class ViewController: UIViewController {
     
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        
-        timerLabel.frame = CGRect (x: 0, y: 0, width: 200, height: 50)
-        timerLabel.text = "00 : 00"
-        timerLabel.layer.cornerRadius = 20
-        timerLabel.backgroundColor = .darkGray
-        timerLabel.textColor = .white
-        
+        createTimerLabel ()
+        createStartButton ()
+        createRoundButton ()
         
         
         view.addSubview(timerLabel)
         view.addSubview(startButton)
         view.addSubview(roundButton)
+
+    }
+
+    func createTimerLabel () {
         
+        timerLabel.frame = CGRect (x: 0, y: 300, width: 200, height: 50)
+        timerLabel.center.x = view.center.x
+        timerLabel.text = "00 : 00"
+        timerLabel.layer.cornerRadius = 20
+        timerLabel.layer.masksToBounds = true
+        timerLabel.backgroundColor = .darkGray
+        timerLabel.textColor = .white
+    }
+    
+    func createStartButton () {
         
+        startButton.frame = CGRect (x: 0, y: 500, width: 200, height: 50)
+        startButton.center.x = view.center.x
+        startButton.setTitle("START", for: .normal)
+        startButton.layer.cornerRadius = 20
+        startButton.layer.masksToBounds = true
+        startButton.backgroundColor = .systemPink
+        startButton.tintColor = .white
         
     }
 
+    func createRoundButton () {
+        
+        startButton.frame = CGRect (x: 0, y: 580, width: 200, height: 50)
+        startButton.center.x = view.center.x
+        startButton.setTitle("NEW ROUND", for: .normal)
+        startButton.layer.cornerRadius = 20
+        startButton.layer.masksToBounds = true
+        startButton.backgroundColor = .systemGreen
+        startButton.tintColor = .white
+        
+    }
 
 }
 
